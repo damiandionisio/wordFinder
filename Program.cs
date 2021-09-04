02e8665241357b494228a538ca77c6ed4cb47107
damiandionisio <31623467+damiandionisio@users.noreply.github.com>
2021-09-04 08:13:21 -0300

    Improving algorithm

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace WordFinder
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
             var wf = new WordFinder(Resources.matrix.Split(",").ToList());
 
             var result = wf.Find(new List<string>
@@ -47,42 +47,27 @@ namespace WordFinder
         {
             var foundWords = new Dictionary<string, int>();
 
-            var matrixLines = this.GetLines();
-
-            foreach (var word in wordstream)
-            {
-                foreach (var line in matrixLines)
-                {
-                    var wordCount = Regex.Matches(line, word).Count;
-                    if(wordCount != 0)
-                        foundWords[word] = !foundWords.ContainsKey(word) ? wordCount : foundWords[word] + wordCount;
-                }
-            }
-
-            return foundWords.OrderByDescending(x => x.Value).Select(x => x.Key).Take(10);
-        }
-        private IEnumerable<string> GetLines()
-        {
-            var lines = new List<string>();
-
-            var verticalWord = new StringBuilder();
-            var horizontalWord = new StringBuilder();
+            var verticalLine = new StringBuilder();
+            var horizontalLine = new StringBuilder();
 
             for (int i = 0; i < matrix.Count; i++)
             {
                 for (int j = 0; j < matrix.Count; j++)
                 {
-                    verticalWord.Append(matrix[j][i]);
-                    horizontalWord.Append(matrix[i][j]);
+                    verticalLine.Append(matrix[j][i]);
+                    horizontalLine.Append(matrix[i][j]);
                 }
-                lines.Add(verticalWord.ToString());
-                lines.Add(horizontalWord.ToString());
-
-                horizontalWord.Clear();
-                verticalWord.Clear();
+                foreach (var word in wordstream)
+                {
+                    var wordCount = Regex.Matches(verticalLine.ToString(), word).Count + Regex.Matches(horizontalLine.ToString(), word).Count;
+                    if (wordCount != 0)
+                        foundWords[word] = !foundWords.ContainsKey(word) ? wordCount : foundWords[word] + wordCount;
+                }
+                horizontalLine.Clear();
+                verticalLine.Clear();
             }
 
-            return lines;
+            return foundWords.OrderByDescending(x => x.Value).Select(x => x.Key).Take(10);
         }
     }
 }
